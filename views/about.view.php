17c4f1ede64696c45942cc551cd7db8b6a643cc4
Andoni Lalanne-Berdouticq <39518930+Akeops@users.noreply.github.com>
2024-09-16 20:47:48 +0200

    Reprise du projet

--- views/about.view.php
+++ views/about.view.php
@@ -11,11 +11,7 @@
      <div class="min-h-full">
           <?php require('partials/nav.php') ?>
 
-          <header class="bg-white shadow">
-               <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-               </div>
-          </header>
+          <?php require 'partials/header.php' ?>
           <main>
                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <!-- Your content -->
