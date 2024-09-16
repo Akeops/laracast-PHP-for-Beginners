caf0d5ea070c717580a6c9997a1f32825adb02aa
Andoni Lalanne-Berdouticq <39518930+Akeops@users.noreply.github.com>
2024-09-16 20:53:09 +0200

    Avancé

--- views/about.view.php
+++ views/about.view.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
-</head>
-
-<body class="h-full">
-     <div class="min-h-full">
-          <?php require('partials/nav.php') ?>
-
-          <?php require 'partials/header.php' ?>
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
           <main>
                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <!-- Your content -->
--- views/contact.view.php
+++ views/contact.view.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
+<?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
-<div class="min-h-full">
 
-
-    <?php require 'partials/header.php' ?>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             hello chad.
